7200806741aeb9c0a0de962cabf5a815f2e04e86
Neba <nebiyudaniel391@gmail.com>
2023-11-02 16:00:21 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Products from './component/Products';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <switch >
+        <Routes exact path="/" component={Home} />
+        <Routes exact path="/products" component={Products} />
+      </switch>
+      <Home />
+      <Products />
+    </>
   );
 }
 
